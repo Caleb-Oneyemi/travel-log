ccde0a5c757d23a19fd5fc8ed2bcd0ec10204107
Caleb-Oneyemi <caleb.oneyemi@decagon.dev>
2021-03-21 19:33:23 +0100

    updates

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
@@ -28,17 +29,19 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-app.get('/', (req, res) => {
-	res.json({
-		message: 'welcome',
-	});
-});
-
 app.use('/api', logRoutes);
 
 app.use(middlewares.notFound);
 
 app.use(middlewares.errorHandler);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
 
 const port = process.env.PORT || 5000;
 
